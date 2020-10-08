84ca2069a237e65910de787eb836f0f9d17e2610
AnbuRaghu <anbalagan28@yahoo.com>
2020-10-08 10:56:13 +0200

    Updates

---
+++ precache-manifest.596ca89e25f31c0655a18b8c3c9e89e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcf2bfeddceddda4f477fdeb11851c8",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "437934898aa09f3ca10b",
+    "url": "/Robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f7c5cd03151a32c86232",
+    "url": "/Robots/static/css/main.5151c07b.chunk.css"
+  },
+  {
+    "revision": "437934898aa09f3ca10b",
+    "url": "/Robots/static/js/2.6fd3e0e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robots/static/js/2.6fd3e0e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c5cd03151a32c86232",
+    "url": "/Robots/static/js/main.02f89c4e.chunk.js"
+  },
+  {
+    "revision": "69e1bef1f9f4c9c0dfda",
+    "url": "/Robots/static/js/runtime-main.5b4823bd.js"
+  }
+]);
